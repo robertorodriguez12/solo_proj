676b71ab0423eb57625c4d3438a0a60bce198ab3
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-10-19 17:42:48 -0600

    Set up all models and digest csv files for project

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Transaction.destroy_all
+InvoiceItem.destroy_all
+Invoice.destroy_all
+Item.destroy_all
+Merchant.destroy_all
+Customer.destroy_all
+require "csv"
+  CSV.foreach(Rails.root.join('db/data/customers.csv'), headers: true) do |row|
+    Customer.create(row.to_h)
+  end
+  CSV.foreach(Rails.root.join('db/data/merchants.csv'), headers: true) do |row|
+    Merchant.create(row.to_h)
+  end
+  CSV.foreach(Rails.root.join('db/data/items.csv'), headers: true) do |row|
+    row["unit_price"] = (row["unit_price"].to_f / 100).round(2)
+    Item.create(row.to_h)
+  end
+  CSV.foreach(Rails.root.join('db/data/invoices.csv'), headers: true) do |row|
+    Invoice.create(row.to_h)
+  end
+  CSV.foreach(Rails.root.join('db/data/invoice_items.csv'), headers: true) do |row|
+    row["unit_price"] = (row["unit_price"].to_f / 100).round(2)
+    InvoiceItem.create(row.to_h)
+  end
+  CSV.foreach(Rails.root.join('db/data/transactions.csv'), headers: true) do |row|
+    Transaction.create(row.to_h)
+  end
+  ActiveRecord::Base.connection.reset_pk_sequence!('customers')
+  ActiveRecord::Base.connection.reset_pk_sequence!('merchants')
+  ActiveRecord::Base.connection.reset_pk_sequence!('items')
+  ActiveRecord::Base.connection.reset_pk_sequence!('invoices')
+  ActiveRecord::Base.connection.reset_pk_sequence!('invoice_items')
+  ActiveRecord::Base.connection.reset_pk_sequence!('transactions')
