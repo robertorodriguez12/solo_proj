b34b1f2f5bd272a99789cde236ea8b3e74c56386
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-10-20 13:39:08 -0600

    Create merchants controller inside ov API V1 directory, merchants request spec test and route.

---
+++ spec/api/v1/merchants_request_spec.rb
@@ -0,0 +1,91 @@
+require 'rails_helper'
+
+RSpec.describe 'MERCHANT API' do
+    it 'sends a list of merchants' do
+        create_list(:merchant, 5)
+
+        get '/api/v1/merchants'
+
+        expect(response).to be_successful
+        
+        json = JSON.parse(response.body, symbolize_names: true)
+        merchants = json[:data]
+
+        expect(merchants).to be_an(Array)
+        expect(merchants.count).to eq(5)
+
+        merchants.each do |merchant|
+            expect(merchant).to have_key(:id)
+            expect(merchant[:id]).to be_an(String)
+
+            expect(merchant).to have_key(:type)
+            expect(merchant[:type]).to be_an(String)
+
+            expect(merchant).to have_key(:attributes)
+            expect(merchant[:attributes]).to be_a(Hash)
+
+            expect(merchant[:attributes]).to have_key(:name)
+            expect(merchant[:attributes][:name]).to be_an(String)
+        end
+    end 
+
+    it 'can get one merchant by its id' do
+        id = create(:merchant).id
+
+        get "/api/v1/merchants/#{id}"
+
+        expect(response).to be_successful
+
+        json = JSON.parse(response.body, symbolize_names: true)
+        merchant = json[:data]
+
+        expect(merchant).to have_key(:id)
+        expect(merchant[:id]).to be_an(String)
+
+        expect(merchant).to have_key(:type)
+        expect(merchant[:type]).to be_an(String)
+
+        expect(merchant).to have_key(:attributes)
+        expect(merchant[:attributes]).to be_a(Hash)
+
+        expect(merchant[:attributes]).to have_key(:name)
+        expect(merchant[:attributes][:name]).to be_an(String)
+    end
+
+    it 'can create a new merchant' do
+        merchant_params = ({name: 'Wacky Daves'})
+        headers = {"CONTENT_TYPE" => "application/json"}
+        post "/api/v1/merchants", headers: headers, params: JSON.generate(merchant_params)
+
+        created_merchant = Merchant.last
+
+        expect(response).to be_successful
+
+        expect(created_merchant.name).to eq(merchant_params[:name])
+    end
+
+    it 'can update an existing merchant' do
+        id = create(:merchant).id
+        previous_name = Merchant.last.name
+
+        merchant_params = { name: "Wacky Daves" }
+        headers = {"CONTENT_TYPE" => "application/json"}
+        patch "/api/v1/merchants/#{id}", headers: headers, params: JSON.generate(merchant_params)
+
+        merchant = Merchant.find(id)
+
+        expect(response).to be_successful
+
+        expect(merchant.name).to_not eq(previous_name)
+        expect(merchant.name).to eq(merchant_params[:name])
+    end
+
+    it 'can destroy a merchant' do
+        merchant = create(:merchant)
+
+        expect{ delete "/api/v1/merchants/#{merchant.id}" }.to change(Merchant, :count).by(-1)
+
+        expect(response).to be_successful
+        expect{Merchant.find(merchant.id)}.to raise_error(ActiveRecord::RecordNotFound)
+    end
+end
