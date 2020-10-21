746e7089b7e32b3745b00e141525a458f9c44aee
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-10-21 15:11:59 -0600

    tests created for section 2c

--- spec/api/v1/items/items_search_spec.rb
+++ spec/api/v1/items/items_search_spec.rb
@@ -4,15 +4,33 @@ RSpec.describe 'ITEMS SEARCH ENDPOINT' do
     it 'can find a list of items that contain a fragment, case insensitive' do
         id_1 = create(:merchant).id
         
-        item1 = { name: 'saw', description: 'sharp', merchant_id: id_1, unit_price: 12.01}
-        item1 = { name: 'saw 2', description: 'extra sharp', merchant_id: id_1, unit_price: 15.01}
-        item1 = { name: 'knife', description: 'a little sharp', merchant_id: id_1, unit_price: 9.01}
+        item1 = Item.create( name: 'saw', description: 'sharp', merchant_id: id_1, unit_price: 12.01)
+        item2 = Item.create( name: 'saw 2', description: 'extra sharp', merchant_id: id_1, unit_price: 15.01)
+        item3 = Item.create( name: 'knife', description: 'a little sharp', merchant_id: id_1, unit_price: 9.01)
 
         get '/api/v1/items/find_all?name=saw'
 
         expect(response).to be_successful
 
-
+        json = JSON.parse(response.body, symbolize_names: true)
+        items = json[:data]
+        expect(items.count).to eq(2)
     end
+
+    it 'can find an item that contains a fragment, case insensitive' do
+        id_1 = create(:merchant).id
+        
+        item1 = Item.create( name: 'saw', description: 'sharp', merchant_id: id_1, unit_price: 12.01)
+        item2 = Item.create( name: 'saw 2', description: 'extra sharp', merchant_id: id_1, unit_price: 15.01)
+        item3 = Item.create( name: 'knife', description: 'a little sharp', merchant_id: id_1, unit_price: 9.01)
+
+        get '/api/v1/items/find?name=saw'
+
+        json = JSON.parse(response.body, symbolize_names: true)
+        name = json[:data][:attributes][:name].downcase
+
+        expect(json[:data]).to be_a(Hash)
+        expect(name).to include('saw')
+    end 
 
 end 
--- spec/api/v1/merchants/merchant_search_spec.rb
+++ spec/api/v1/merchants/merchant_search_spec.rb
@@ -2,12 +2,30 @@ require 'rails_helper'
 
 RSpec.describe 'MERCHANTS SEARCH ENDPOINT' do
     it 'can return a list of merchants that contain a fragment, case insensitive' do
-        merchant = Merchant.create!{name: 'The Juan'}
-        merchant2 = Merchant.create!{name: 'Not Juan'}
-        merchant3 = Merchant.create!{name: 'Maybe Juan'}
+        merchant = Merchant.create!(name: 'The Juan')
+        merchant2 = Merchant.create!(name: 'Not Juan')
+        merchant3 = Merchant.create!(name: 'Maybe Juan')
 
-        get '/api/v1/merchants/find_all?name=saw'
+        get '/api/v1/merchants/find_all?name=juan'
 
         expect(response).to be_successful
+        json = JSON.parse(response.body, symbolize_names: true)
+        merchants = json[:data]
+
+        expect(merchants.count).to eq(3)
     end
+
+    it 'can return a single merchant that contains a fragment, case insensitive' do
+        merchant = Merchant.create!(name: 'The Juan')
+        merchant2 = Merchant.create!(name: 'Not Juan')
+        merchant3 = Merchant.create!(name: 'Maybe Juan')
+
+        get '/api/v1/merchants/find?name=juan'
+
+        json = JSON.parse(response.body, symbolize_names: true)
+        name = json[:data][:attributes][:name].downcase
+
+        expect(json[:data]).to be_a(Hash)
+        expect(name).to include('juan')
+    end 
 end
