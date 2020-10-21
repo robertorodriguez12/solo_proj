d3f1d8b3a9960b700f13d86ee0e221f8bf4a645e
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-10-21 15:10:51 -0600

    Add relationship to transactions and create a class method that was needed for section 2c

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,5 +4,9 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
-  
+  has_many :transactions, through: :invoices
+
+  def self.find_by_name(name)
+    items = Item.where("lower(name) LIKE ?", "%" + name.downcase + "%")
+  end
 end
