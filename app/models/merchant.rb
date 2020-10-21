8536b0ee1fc3d5c92118b44a7de7e4cc6c432d98
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-10-21 15:11:45 -0600

    Create class method for section 2c

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,8 @@ class Merchant < ApplicationRecord
     has_many :items
     has_many :invoices
     has_many :customers, through: :invoices
+
+    def self.find_by_name(name)
+        merchant = Merchant.where("lower(name) LIKE ?", "%" + name.downcase + "%")
+    end
 end
