3dbc5c4706dce8f4994224c0d98c39b2181e693f
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-10-21 15:10:25 -0600

    Adds relationship to transactions

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -7,5 +7,6 @@ RSpec.describe Invoice, type: :model do
   describe "relationships" do
     it {should belong_to(:customer)}
     it {should belong_to(:merchant)}
+    it {should have_many(:transactions)}
   end 
 end
