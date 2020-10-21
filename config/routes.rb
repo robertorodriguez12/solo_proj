de17ad8794c6fc7ceb46d84440e10e238ba755fe
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-10-21 15:12:07 -0600

    Routes added for section 2c

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,20 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+
+      namespace :merchants do
+        get '/:id/items', to: 'items#index'
+        get '/find_all', to: 'search#index'
+        get '/find', to: 'search#show'      
+      end
+
+      namespace :items do
+        get '/find_all', to: 'search#index'
+        get '/:id/merchant', to: 'merchants#show'
+        get '/find', to: 'search#show'
+      end
+
+
       resources :merchants
       resources :items
     end 
