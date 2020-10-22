274d4df5cd6d9701ecd218eadcaab6f7bbfa4cc1
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-10-22 10:52:37 -0600

    Add additional relationships in order to make most revenue method work

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -8,5 +8,7 @@ RSpec.describe Invoice, type: :model do
     it {should belong_to(:customer)}
     it {should belong_to(:merchant)}
     it {should have_many(:transactions)}
+    it {should have_many(:invoice_items)}
+    it {should have_many(:items)}
   end 
 end
