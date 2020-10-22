c6138ccc50d35ce0651f4ba8d51c5b51a88bd998
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-10-22 11:34:56 -0600

    Revert "Fix relationship issues that were causing invoice_spec test to pass"
    This reverts commit fe331726d639181ce530de27dbf7436c1c31dfd1.

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -9,6 +9,6 @@ RSpec.describe Invoice, type: :model do
     it {should belong_to(:merchant)}
     it {should have_many(:transactions)}
     it {should have_many(:invoice_items)}
-    it {should have_many(:items).through(:invoice_items)}
+    it {should have_many(:items)}
   end 
 end
